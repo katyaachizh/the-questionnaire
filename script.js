a8bd15e34280a8dce8ca27cf03a084a4a1d03616
katyaachizh <141716784+katyaachizh@users.noreply.github.com>
2023-10-25 15:00:46 +0300

    hometask-10
    все исправила!

--- script.js
+++ script.js
@@ -1,8 +1,40 @@
 const form = document.querySelector(".form");
-form.addEventListener("submit", (event) => {
-  // Предотвращает действие браузера по умолчанию. В данном случае — отправку формы
-  // https://learn.javascript.ru/default-browser-action
-  event.preventDefault();
+const userName = document.querySelector('#name');
+const userSecondName = document.querySelector('#secondName');
+const userEmail = document.querySelector('#email');
+const userPhone = document.querySelector('#phone');
+const userAgree = document.querySelector('#agree');
+const sendButton = document.querySelector('#button');
+const clearButton = document.querySelector('#clear');
 
-  // Здесь твой код
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  
+  fetch(`https://polinashneider.space/user`, {
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer: katyaachizh'
+    },
+    body: JSON.stringify({
+      name: `${userName.value}`,
+      secondName: `${userSecondName.value}`,
+      email: `${userEmail.value}`,
+      phone: `${userPhone.value}`,
+      agree: `${userAgree.checked}`
+    }),
+  })
+  
+  .then((result) => {
+    return result.json();
+  })
+  .then((data) => {
+    console.log(data);
+    alert('Регистрация прошла успешно!');
+    form.reset()
+  })
+  .catch((err) => {
+    alert('Ошибка!');
+  });
 });
